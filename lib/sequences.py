8ef87895c8bc92b287bda7ee2567a451850acc2b
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-08-19 12:59:50 +0300

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
-    fibonacci_sequence = []
+    fibonacci = []
     if length > 0:
-        fibonacci_sequence.append(0)
-        if length > 1:
-            fibonacci_sequence.append(1)
-            if length > 2:
-                for i in range(2, length):
-                    fibonacci_sequence.append(
-                        fibonacci_sequence[i - 1] + fibonacci_sequence[i - 2])
+        fibonacci.append(0)
+    if length > 1:
+        fibonacci.append(1)
+    if length > 2:
+        for i in range(2, length):
+            fibonacci.append(fibonacci[i - 1] + fibonacci[i - 2])
 
-    print(fibonacci_sequence)
-
+    print(fibonacci)
